6a8766a2a6f3ac9d50b0091347421be40514528d
jasoyode <jasoyode@indiana.edu>
2017-06-30 18:04:14 -0400

    fixing  third test package, but makign test fail

--- src/test/java/org/jacoco/examples/maven/java/HelloWorld3Test.java
+++ src/test/java/org/jacoco/examples/maven/java/HelloWorld3Test.java
@@ -1,4 +1,4 @@
-package org.jacoco.examples.maven;
+package org.jacoco.examples.maven.java;
 
 import static org.junit.Assert.*;
 
@@ -16,7 +16,7 @@ public class HelloWorld3Test {
 	
 	@Test
 	public void testGetMessage() {
-		assertEquals("Hello World!", subject.getMessage(false));
+		assertEquals("Hello World.", subject.getMessage(false));
 	}
 	
 }
