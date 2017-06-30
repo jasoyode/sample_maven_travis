2d5cbe0ec570217bdb907777d9b359d75c3976b5
jasoyode <jasoyode@indiana.edu>
2017-06-30 17:57:41 -0400

    adding fxing  test to see if it works

--- src/test/java/org/jacoco/examples/maven/java/HelloWorld2Test.java
+++ src/test/java/org/jacoco/examples/maven/java/HelloWorld2Test.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class HelloWorldTest2 {
+public class HelloWorld2Test {
 
 	private HelloWorld subject;
 
