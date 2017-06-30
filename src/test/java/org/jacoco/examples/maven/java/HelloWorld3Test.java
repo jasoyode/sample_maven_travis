e7c4cbf2f70ffc41908becdd21b9edf3b99d3202
jasoyode <jasoyode@indiana.edu>
2017-06-30 18:03:47 -0400

    fixing  third test name but leaving bad pacakge in

--- src/test/java/org/jacoco/examples/maven/java/HelloWorld3Test.java
+++ src/test/java/org/jacoco/examples/maven/java/HelloWorld3Test.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class HelloWorldTest3 {
+public class HelloWorld3Test {
 
 	private HelloWorld subject;
 
